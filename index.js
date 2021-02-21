24502f194441ce5142b9db0607af26019a73dc01
Your Name <you@example.com>
2021-02-21 10:04:59 +0100

    update for omni & UEI

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send(`
 <body>
 
 <form action="/post" method="POST">
-<textarea type="textarea" name="data" style="width:100%;height:20%" placeholder="Omni inputXML"></textarea>
+<textarea type="textarea" name="data" style="width:100%;height:20%" placeholder="UEI/Omni inputXML"></textarea>
    <button type="submit">Submit</button>
 </form>
 
@@ -35,41 +35,53 @@ app.get('/', function(req, res) {
 })
 
 
-app.post('/post', function(req, res) {
+app.post('/post', function (req, res) {
 
     var child = spawn('/root/edid/edid-decode/edid-decode', ["-c", "--skip-sha", "-"]);
 
     child.stdin.setEncoding('utf-8');
 
-var outputdata = "<html><body style='word-wrap: break-word;white-space: pre-wrap;'>"
+    var outputdata = "<html><body style='word-wrap: break-word;white-space: pre-wrap;'>"
 
-outputdata += `
+    outputdata += `
 <form action="/post" method="POST">
-<textarea type="textarea" name="data" style="width:100%;height:20%" placeholder="Omni inputXML"></textarea>
+<textarea type="textarea" name="data" style="width:100%;height:20%" placeholder="UEI/Omni inputXML"></textarea>
    <button type="submit">Submit</button>
 </form>
 
 `
 
 
-var inputdata = req.body.data;
-inputdata = Buffer.from(inputdata, 'base64').toString('utf-8')
-//outputdata += inputdata + "<br /><br />";
+    var inputdata = req.body.data;
 
-    var a = inputdata.indexOf("<edid>");
-    var b = inputdata.indexOf("</edid>", a+1);
+if(inputdata.indexOf("</edid>") > -1){
+    var isEUI = true;
+}
 
-inputdata = inputdata.substr(a+6, b-a-6);
-outputdata += inputdata + "<br /><br />"
+if(!isEUI){
+    inputdata = Buffer.from(inputdata, 'base64').toString('utf-8') //b64 decode
+}
+  
+    //outputdata += inputdata + "<br /><br />";
 
-inputdata = Buffer.from(inputdata, 'base64')
+    //remove edids
+    var a = inputdata.indexOf("<edid");
+    var b = inputdata.indexOf("</edid>", a + 1);
+if(isEUI){
+    inputdata = inputdata.substr(a + 13, b - a - 6);
+}else{
+    inputdata = inputdata.substr(a + 6, b - a - 6);
+}
+   
 
-console.log(inputdata)
+    //base64, what was inside of <edid></edid>
+    outputdata += inputdata + "<br /><br />" //
 
-//outputdata += inputdata + "<br /><br />";
+    inputdata = Buffer.from(inputdata, 'base64')
 
+    console.log(inputdata)
 
-
+    //outputdata += inputdata + "<br /><br />";
 
     child.stdin.write(inputdata);
 
@@ -77,16 +89,16 @@ console.log(inputdata)
 
     var databuf;
 
-    child.stdout.on('data', function(data) {
+    child.stdout.on('data', function (data) {
         console.log(data)
         databuf += data;
 
     })
 
-    child.stdout.on('end', function(data) {
+    child.stdout.on('end', function (data) {
         console.log(databuf);
 
-outputdata += databuf + "</body></html>";
+        outputdata += databuf + "</body></html>";
 
         res.send(outputdata)
     })
